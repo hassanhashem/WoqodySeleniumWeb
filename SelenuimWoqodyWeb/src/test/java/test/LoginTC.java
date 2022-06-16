961dbd0372678700fca92a34553b169028338707
hassanhashem <hassan.hashem.74@gmail.com>
2022-06-16 04:37:36 +0200

    Login test case using testNG and POM

--- SelenuimWoqodyWeb/src/test/java/test/LoginTC.java
+++ SelenuimWoqodyWeb/src/test/java/test/LoginTC.java
@@ -1,20 +1,28 @@
 package test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import Pages.LoginPage;
 //-----------------------------------------------------------------------------------
 //start of test case
 public class LoginTC 
 {
-	private static WebDriver driver = null;
-	public static void main(String[] args)
-		{
-			System.setProperty("webdriver.chrome.driver","D:\\Automation\\SelenuimWoqodyWeb\\drivers\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			driver.get("https://staging-admin.woqody.tech/login");
-			LoginPage.loginusername(driver).sendKeys("admin@woqody-staging.tech");
-			LoginPage.loginpassword(driver).sendKeys("@P@2CK550OQylm");
-			LoginPage.loginbutton(driver).click();
-		}	
+	WebDriver driver = null;
+			@BeforeTest
+			public void setupTest() 
+				{
+				System.setProperty("webdriver.chrome.driver","D:\\Automation\\SelenuimWoqodyWeb\\drivers\\chromedriver.exe");
+				driver = new ChromeDriver();
+				}	
+			@Test
+			public void login()
+				{
+					driver.get("https://staging-admin.woqody.tech/login");
+					LoginPage.loginusername(driver).sendKeys("admin@woqody-staging.tech");
+					LoginPage.loginpassword(driver).sendKeys("@P@2CK550OQylm");
+					LoginPage.loginbutton(driver).click();
+				}	
 //end of test case
 }
